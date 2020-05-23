156c43e14e7d9b9674c278426fdf0500eec231b4
Prabath Shalitha <prabathshalitha21@gmail.com>
2020-05-24 03:04:28 +0530

    2

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,4 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
